2431d3f69c435679b20916f9f35368b7bdecac12
Choi Doong <dmsalstjdals@naver.com>
2023-12-22 09:50:41 +0900

    10926번

---
+++ SungMin23_12_21_5/src/SungMin/SungMin.java
@@ -0,0 +1,16 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin{
+    public static void main(String[] args){
+        int sung;
+        String sungMin;
+
+        Scanner sc = new Scanner(System.in);
+        sungMin = sc.nextLine();
+
+        System.out.print(sungMin+"??!");
+        
+    }
+}
