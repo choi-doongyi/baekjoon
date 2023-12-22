d26b042ad3e72c6807eabb06ad340ddb05bf4e98
Choi Doong <dmsalstjdals@naver.com>
2023-12-22 09:59:11 +0900

    18108

---
+++ SungMin23_12_22_01/src/SungMin/SungMin.java
@@ -0,0 +1,12 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = Integer.parseInt(sc.nextLine());
+		System.out.print(a-543);
+	}
+}
