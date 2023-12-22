dc222c6414d848f5a1d87270c2d66cf297824ddd
Choi Doong <dmsalstjdals@naver.com>
2023-12-22 21:40:07 +0900

    11382번 꼬마 정민

---
+++ SungMin23_12_22_04/src/SungMin/SungMin.java
@@ -0,0 +1,18 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin{
+    public static void main(String[] args){
+        Long sungMin,sungMin2,sungMin3;
+
+        Scanner sc = new Scanner(System.in);
+        sungMin = sc.nextLong();
+        sungMin2 = sc.nextLong();
+        sungMin3 = sc.nextLong();
+
+        System.out.println(sungMin + sungMin2 + sungMin3);
+
+        
+    }
+}
