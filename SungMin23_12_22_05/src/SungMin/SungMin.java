1c9b19693990a814a238def84e0094dd0ab10c84
Choi Doong <dmsalstjdals@naver.com>
2023-12-22 21:43:16 +0900

    10171 고양이

---
+++ SungMin23_12_22_05/src/SungMin/SungMin.java
@@ -0,0 +1,14 @@
+package SungMin;
+
+public class SungMin {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");;
+
+		 
+		
+		 
+	}
+}
