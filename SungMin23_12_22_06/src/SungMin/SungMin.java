865fa521aad5d4a6df1ab1dd930d41bafd621163
Choi Doong <dmsalstjdals@naver.com>
2023-12-22 21:48:00 +0900

    10172 개

---
+++ SungMin23_12_22_06/src/SungMin/SungMin.java
@@ -0,0 +1,11 @@
+package SungMin;
+
+public class SungMin {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
