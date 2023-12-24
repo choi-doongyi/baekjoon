5e7b5b8735d41f88612d9f5df9fb2aa2f03d8924
Choi Doong <dmsalstjdals@naver.com>
2023-12-24 23:06:12 +0900

    2525 오븐 시계

--- SungMin23_23_06/src/SungMin/SungMin.java
+++ SungMin23_23_06/src/SungMin/SungMin.java
@@ -9,7 +9,6 @@ public class SungMin {
 		int a = Integer.parseInt(sc.next());
 		int b = Integer.parseInt(sc.next());
 		int c = Integer.parseInt(sc.next());
-		
 		if(b+c>=60) {
 			if((a+((b+c)/60))<=23) {
 				System.out.println((a+((b+c)/60))+" "+((b+c)%60));
