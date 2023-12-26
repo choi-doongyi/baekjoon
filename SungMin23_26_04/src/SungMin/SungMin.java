37dd33261e80e23f9a9ce1dece5cc2b97bab06b2
Choi Doong <dmsalstjdals@naver.com>
2023-12-26 14:45:16 +0900

    11654 아스키 코드

---
+++ SungMin23_26_04/src/SungMin/SungMin.java
@@ -0,0 +1,13 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char a = sc.next().charAt(0);
+		int b = (int)a;
+		System.out.print(b);
+	}
+}
