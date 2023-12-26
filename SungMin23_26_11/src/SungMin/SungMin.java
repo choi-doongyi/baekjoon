86c8fb82991a4f4211d57e38b18abb76035a64f3
Choi Doong <dmsalstjdals@naver.com>
2023-12-26 14:47:09 +0900

    11718 그대로 출력하기

---
+++ SungMin23_26_11/src/SungMin/SungMin.java
@@ -0,0 +1,14 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			String a = sc.nextLine();
+			System.out.println(a);
+		}	
+	}
+}
