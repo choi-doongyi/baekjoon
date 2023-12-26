d9c1bfd18e4a872e56d013b1b4abec4a7decab65
Choi Doong <dmsalstjdals@naver.com>
2023-12-26 14:44:37 +0900

    2743 단어 길이 재기

---
+++ SungMin23_26_02/src/SungMin/SungMin.java
@@ -0,0 +1,14 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		System.out.print(a.length());
+
+		
+	}
+}
