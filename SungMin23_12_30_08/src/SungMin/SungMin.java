d8d04a12a13c8a590b799f6f55af785cc7806a71
Choi Doong <dmsalstjdals@naver.com>
2023-12-30 22:04:50 +0900

    15894 수학은 체육과목 입니다.

---
+++ SungMin23_12_30_08/src/SungMin/SungMin.java
@@ -0,0 +1,15 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = Long.parseLong(sc.next());
+		
+		
+		System.out.println(a*4);
+		
+		
+	}
+}
