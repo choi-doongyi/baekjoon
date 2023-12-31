64d0957cb1759e85d61a409cbc274d43c775695d
Choi Doong <dmsalstjdals@naver.com>
2023-12-31 22:53:40 +0900

    24264 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ SungMin23_12_31_07/src/SungMin/SungMin.java
@@ -0,0 +1,12 @@
+package SungMin;
+
+import java.util.Scanner;
+
+public class SungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = Long.parseLong(sc.next());
+		System.out.println(n*n);
+		System.out.println(2);
+	}
+}
