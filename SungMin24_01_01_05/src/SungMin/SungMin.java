76c805bfef7847c67b61224cb4849b2b98778705
Choi Doong <dmsalstjdals@naver.com>
2024-01-01 23:36:00 +0900

    2839 설탕 배달

--- SungMin24_01_01_05/src/SungMin/SungMin.java
+++ SungMin24_01_01_05/src/SungMin/SungMin.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class SungMin {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int a = Integer.parseInt(sc.next());
 		int sungmin = a%10;
 		if(a%5==0) {
