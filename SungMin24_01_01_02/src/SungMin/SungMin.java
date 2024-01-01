6ec20601c388f2ebf4b015e16dcc19830ed989c5
Choi Doong <dmsalstjdals@naver.com>
2024-01-01 23:35:00 +0900

    19532 수학은 비대면강의입니다

--- SungMin24_01_01_02/src/SungMin/SungMin.java
+++ SungMin24_01_01_02/src/SungMin/SungMin.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class SungMin {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int a =Integer.parseInt(sc.next());
 		int b =Integer.parseInt(sc.next());
 		int c =Integer.parseInt(sc.next());
