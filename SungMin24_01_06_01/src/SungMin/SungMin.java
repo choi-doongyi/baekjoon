6b5d049d9ee1aa47d94362c6503c6e1e5ab971d2
Choi Doong <dmsalstjdals@naver.com>
2024-01-06 23:52:40 +0900

    11866 요세푸스 문제 0

---
+++ SungMin24_01_06_01/src/SungMin/SungMin.java
@@ -0,0 +1,32 @@
+package SungMin;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class SungMin {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//11050 이항 계수 1
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+ 
+		System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
+	}
+ 
+	static int factorial(int N) {
+		if (N <= 1)	{
+			return 1;
+		}
+		return N * factorial(N - 1);
+	}
+}
