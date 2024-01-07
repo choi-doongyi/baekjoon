0947eb8aeade7fb887d79326a8f6b8b5a64bbe19
Choi Doong <dmsalstjdals@naver.com>
2024-01-07 21:03:17 +0900

    1676 팩토리얼 0의 개수

---
+++ SungMin24_01_07_02/src/SungMin/SungMin.java
@@ -0,0 +1,29 @@
+package SungMin;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class SungMin {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a =Integer.parseInt(bf.readLine());
+
+		System.out.println(a/5+a/25+a/125);
+		
+		//1676 팩토리얼 0의 개수
+
+		
+	
+		bw.close();
+		
+	}
+}
