dc7d5ed913e7108625ca52d6a134fddac59c2775
Choi Doong <dmsalstjdals@naver.com>
2024-02-27 13:05:01 +0900

    2338 긴자리 계산

---
+++ SungMin24_02_27_02/src/SungMin/SungMin.java
@@ -0,0 +1,31 @@
+package SungMin;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+ 
+public class SungMin {
+	//2338 긴자리 계산
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BigInteger a = new BigInteger(bf.readLine());
+		BigInteger b = new BigInteger(bf.readLine());
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+}
+
