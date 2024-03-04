a82b4a6f57aaad3a14fa64b78e0774a9b6b0aca1
Choi Doong <dmsalstjdals@naver.com>
2024-03-04 21:15:55 +0900

    15652 N과 M(4)

---
+++ SungMin24_03_04_01/src/SungMin/SungMin.java
@@ -0,0 +1,54 @@
+package SungMin;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+ 
+public class SungMin {
+	//15652 N과 M(4)
+	static int n, m;
+	public static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+        
+		dfs(1, 0);
+		System.out.print(sb);
+	}
+	
+	public static void dfs(int at, int depth) {
+		 
+		if (depth == m) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+        
+		for (int i = at; i <= n; i++) {
+ 
+			arr[depth] = i;
+			dfs(i , depth + 1);
+ 
+		}
+	}
+}
+
