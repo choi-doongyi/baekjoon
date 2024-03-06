23b5dcb8b9f7e89fec3002bbb0f5dc30b645da19
Choi Doong <dmsalstjdals@naver.com>
2024-03-06 19:18:23 +0900

    11725 트리의 부모 찾기

---
+++ SungMin24_03_06_01/src/SungMin/SungMin.java
@@ -0,0 +1,65 @@
+package SungMin;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class SungMin {
+	//11725 트리의 부모 찾기
+	static int t;
+	static int[][] one;
+    static boolean[] check;
+    static HashMap<Integer, Integer> h1 = new HashMap<Integer,Integer>();
+    static Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 노드 개수 입력
+
+		// 트리 구조 표현을 위한 그래프 구현
+		ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+		for (int i = 0; i < n; i++)
+			tree.add(new ArrayList<>());
+
+		// 그래프 입력
+		for (int i = 0; i < n - 1; i++) {
+			int n1 = sc.nextInt() - 1;
+			int n2 = sc.nextInt() - 1;
+			tree.get(n1).add(n2);
+			tree.get(n2).add(n1);
+		}
+
+		boolean[] visited = new boolean[n]; // 방문 여부 확인용 배열
+		int[] parentNode = new int[n]; // 부모 노드 저장 배열
+
+		// BFS
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(0);
+		visited[0] = true;
+		while (!queue.isEmpty()) {
+			int v = queue.remove();
+			for (int node : tree.get(v))
+				if (!visited[node]) {
+					visited[node] = true;
+					queue.add(node);
+					parentNode[node] = v; // 부모 노드 찾은 후 저장
+				}
+		}
+
+		// 루트 노드를 제외한 나머지 노드의 부모 노드 출력
+		for (int i = 1; i < n; i++)
+			System.out.println(parentNode[i] + 1);
+	}
+}
+
