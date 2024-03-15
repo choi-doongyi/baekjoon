46ea1f807e02712c97e2abf3be6573d37a25e44f
Choi Doong <dmsalstjdals@naver.com>
2024-03-15 21:47:05 +0900

    5639 이진 검색 트리

---
+++ SungMin24_03_15_01/src/SungMin/SungMin.java
@@ -0,0 +1,59 @@
+package SungMin;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+ 
+public class SungMin {
+	//5639 이진 검색 트리
+	static class Node {
+        int num;
+        Node left, right;
+
+        Node(int num) {
+            this.num = num;
+        }
+
+        Node(int num, Node left, Node right) {
+            this.num = num;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int n) {
+            if (n < this.num) {
+                if (this.left == null)
+                    this.left = new Node(n);
+                else this.left.insert(n);
+            } else {
+                if (this.right == null)
+                    this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Node root = new Node(Integer.parseInt(bf.readLine()));
+        String input;
+        while (true) {
+            input = bf.readLine();
+            if (input == null || input.equals(""))
+                break;
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+	static void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.num);
+    }
+}
