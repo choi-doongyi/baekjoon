ff4d263a9cee6a6e111a51a35fb1f8b066ac0794
Choi Doong <dmsalstjdals@naver.com>
2024-04-03 20:32:52 +0900

    15649 N과 M (1)

---
+++ SungMin24_04_03_01/src/SungMin/SungMin.java
@@ -0,0 +1,46 @@
+package SungMin;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class SungMin {
+	//15649 N과 M (1)
+	static int N, M;
+	public static int[] arr;
+	public static boolean[] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(bf.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visit = new boolean[N];
+		dfs(N, M, 0);
+	}
+	public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				dfs(N, M, depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
+}
